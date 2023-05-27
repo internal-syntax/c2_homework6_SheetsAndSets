266aa628ceb0f77a6b5e3085e66b614d10dfa9c4
Aleksandr <sandro-wold@live.ru>
2023-05-27 12:49:50 +0500

    Initial commit

---
+++ src/main/java/com/example/c2_homework6_sheetsandsets/C2Homework6SheetsAndSetsApplication.java
@@ -0,0 +1,13 @@
+package com.example.c2_homework6_sheetsandsets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C2Homework6SheetsAndSetsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(C2Homework6SheetsAndSetsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/c2_homework6_sheetsandsets/C2Homework6SheetsAndSetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.c2_homework6_sheetsandsets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class C2Homework6SheetsAndSetsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
